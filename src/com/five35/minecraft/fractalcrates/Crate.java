7bc8706828e463fa714ba255d08b99f9386e8ea9
Ben Blank <ben.blank@gmail.com>
2013-08-22 14:34:34 -0700

    add basic (non-functional) crate block

---
+++ src/com/five35/minecraft/fractalcrates/Crate.java
@@ -0,0 +1,26 @@
+package com.five35.minecraft.fractalcrates;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockContainer;
+import net.minecraft.block.material.Material;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.World;
+
+public class Crate extends BlockContainer {
+	public Crate(final int id) {
+		super(id, Material.wood);
+
+		this.setCreativeTab(CreativeTabs.tabDecorations);
+		this.setHardness(2);
+		this.setResistance(10);
+		this.setStepSound(Block.soundWoodFootstep);
+		this.setUnlocalizedName("fractalCrate");
+	}
+
+	@Override
+	public TileEntity createNewTileEntity(final World world) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
--- src/com/five35/minecraft/fractalcrates/FractalCrates.java
+++ src/com/five35/minecraft/fractalcrates/FractalCrates.java
@@ -1,11 +1,14 @@
 package com.five35.minecraft.fractalcrates;
 
-import cpw.mods.fml.common.event.FMLInitializationEvent;
-import cpw.mods.fml.common.Mod.EventHandler;
-import cpw.mods.fml.common.event.FMLPreInitializationEvent;
-import cpw.mods.fml.common.Mod.Instance;
-import net.minecraftforge.common.Configuration;
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.common.Configuration;
+import net.minecraftforge.oredict.ShapedOreRecipe;
 
 @Mod(modid = "FractalCrates")
 public class FractalCrates {
@@ -13,18 +16,24 @@ public class FractalCrates {
 	public static FractalCrates instance;
 
 	static Configuration config;
+	static int crateID;
+	public static Crate crate;
+
+	@EventHandler
+	public static void init(@SuppressWarnings("unused") final FMLInitializationEvent event) {
+		FractalCrates.crate = new Crate(FractalCrates.crateID);
+		GameRegistry.registerBlock(FractalCrates.crate, FractalCrates.crate.getUnlocalizedName());
+
+		GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(FractalCrates.crate), new String[] { "x x", "x x", "xxx" }, Character.valueOf('x'), "plankWood"));
+	}
 
 	@EventHandler
 	public static void preInit(final FMLPreInitializationEvent event) {
 		FractalCrates.config = new Configuration(event.getSuggestedConfigurationFile());
 		FractalCrates.config.load();
 
-		// config options go here
+		FractalCrates.crateID = FractalCrates.config.getBlock("crate", 2083).getInt();
 
 		FractalCrates.config.save(); // if config file was missing, this will write the defaults
-	}
-
-	@EventHandler
-	public void init(@SuppressWarnings("unused") final FMLInitializationEvent event) {
 	}
 }
