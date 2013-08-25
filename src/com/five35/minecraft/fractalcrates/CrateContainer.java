37d816047be3ecb431371b73317a4e5cf5c8ead7
Ben Blank <ben.blank@gmail.com>
2013-08-25 13:42:55 -0700

    forgot about transferStackInSlot

--- src/com/five35/minecraft/fractalcrates/CrateContainer.java
+++ src/com/five35/minecraft/fractalcrates/CrateContainer.java
@@ -3,6 +3,7 @@ package com.five35.minecraft.fractalcrates;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.Container;
 import net.minecraft.inventory.Slot;
+import net.minecraft.item.ItemStack;
 
 public class CrateContainer extends Container {
 	final static int WIDTH = 176;
@@ -32,5 +33,45 @@ public class CrateContainer extends Container {
 	@Override
 	public boolean canInteractWith(@SuppressWarnings("hiding") final EntityPlayer player) {
 		return this.crate.isUseableByPlayer(player);
+	}
+
+	@Override
+	public ItemStack transferStackInSlot(@SuppressWarnings("hiding") final EntityPlayer player, final int slotIndex) {
+		final Slot slot = (Slot) this.inventorySlots.get(slotIndex);
+		ItemStack result = null;
+
+		if (slot != null && slot.getHasStack()) {
+			final ItemStack stack = slot.getStack();
+
+			result = stack.copy();
+
+			if (slotIndex == 0) {
+				// coming from crate
+
+				if (!this.mergeItemStack(stack, 1, this.inventorySlots.size(), true)) {
+					return null;
+				}
+			} else {
+				// going into crate
+
+				if (!this.mergeItemStack(stack, 0, 1, false)) {
+					return null;
+				}
+			}
+
+			if (stack.stackSize == 0) {
+				slot.putStack(null);
+			} else {
+				slot.onSlotChanged();
+			}
+
+			if (stack.stackSize == result.stackSize) {
+				return null;
+			}
+
+			slot.onPickupFromSlot(player, stack);
+		}
+
+		return result;
 	}
 }
